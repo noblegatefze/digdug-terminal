144025829056bc20b0434a93f7f0e3f802b4a0c1
Matt <matt@digdug.do>
2026-01-30 14:01:21 +0400

    chore: bump version to v0.2.0.14

--- app/lib/build.ts
+++ app/lib/build.ts
@@ -2,6 +2,6 @@
 // Update ONLY APP_VERSION when bumping versions.
 
 export const PHASE_LABEL = "Zero Phase";
-export const APP_VERSION = "v0.2.0.13";
+export const APP_VERSION = "v0.2.0.14";
 
 export const DISPLAY_VERSION = `${PHASE_LABEL} ${APP_VERSION}`;
